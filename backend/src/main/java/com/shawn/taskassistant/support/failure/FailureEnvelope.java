168c8e8c76399e515b3847dea18e35c0349df5b4
CauliflowerEater <149353915+CauliflowerEater@users.noreply.github.com>
2026-01-21 16:16:58 +0800

    refactor[backend]:更新failureCertainty类引用

--- backend/src/main/java/com/shawn/taskassistant/support/failure/FailureEnvelope.java
+++ backend/src/main/java/com/shawn/taskassistant/support/failure/FailureEnvelope.java
@@ -1,6 +1,7 @@
 package com.shawn.taskassistant.support.failure;
 
 import java.util.Map;
+import com.shawn.taskassistant.shared.failure.FailureCertainty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- backend/src/main/java/com/shawn/taskassistant/support/failure/SystemFailure.java
+++ backend/src/main/java/com/shawn/taskassistant/support/failure/SystemFailure.java
@@ -1,5 +1,6 @@
 package com.shawn.taskassistant.support.failure;
 
+import com.shawn.taskassistant.shared.failure.FailureCertainty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
