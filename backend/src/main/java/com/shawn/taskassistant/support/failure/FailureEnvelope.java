864175a4a73c049d70b843101491b8a38933e508
CauliflowerEater <149353915+CauliflowerEater@users.noreply.github.com>
2026-02-01 20:11:44 +0800

    refactor[backend]:简化usecase异常字段；

--- backend/src/main/java/com/shawn/taskassistant/support/failure/FailureEnvelope.java
+++ backend/src/main/java/com/shawn/taskassistant/support/failure/FailureEnvelope.java
@@ -1,6 +1,5 @@
 package com.shawn.taskassistant.support.failure;
 
-import java.util.Map;
 import com.shawn.taskassistant.shared.failure.FailureCertainty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,11 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FailureEnvelope {
-    private String usecaseSemantic;
     private FailureSemantic failureSemantic;
     private FailureCertainty certainty;
     private String traceId;
     private String occurredAt;
-    private Map<String, Object> context;
 }
 
